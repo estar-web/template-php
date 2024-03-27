bef1f23b7d73922454f5eb21b7cccbf54bb1cfe0
ほし <cgastrogation@gmail.com>
2024-03-27 10:16:05 +0900

    [init]_initial commit

---
+++ _src/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja" prefix="og: https://ogp.me/ns#">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=1">
+  <title>タイトル</title>
+  <meta name="description" content="(ページの説明)">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="format-detection" content="telephone=no,address=no,email=no">
+  <!-- ogp -->
+  <meta property="og:url" content="">
+  <meta property="og:title" content="">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="">
+  <meta property="og:image" content="">
+  <meta property="og:site_name" content="">
+  <meta property="og:locale" content="ja_JP">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="">
+  <!-- ogp -->
+  <link rel="canonical" href="正規ページのURL">
+  <link rel="icon" href=""><!-- ファビコン -->
+  <link rel="icon" href="./icon.svg" type="image/svg+xml"><!-- モダンブラウザ用SVGアイコン -->
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png"><!-- スマホホーム画面ショートカット用アイコン -->
+  <link rel="manifest" href="./manifest.webmanifest"><!-- Android用pngアイコンマニフェスト -->
+  <?php wp_head(); ?>
+</head>
+<body>
+  <header id="header" class="l-header">
+
+  </header>
